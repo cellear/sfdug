cfbeead1d8daec88d4894bc1efe6b312f41e1b02
Luke McCormick <lm2000@kiza.com>
2026-01-04 13:32:23 -0800

    Add Pantheon settings inclusion to settings.php

--- drupalcms/web/sites/default/settings.php
+++ drupalcms/web/sites/default/settings.php
@@ -881,6 +881,15 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 }
 
 /**
+ * Pantheon Settings
+ */
+if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+  if (file_exists(__DIR__ . '/settings.pantheon.php')) {
+    include __DIR__ . '/settings.pantheon.php';
+  }
+}
+
+/**
  * Load local development override configuration, if available.
  *
  * Create a settings.local.php file to override variables on secondary (staging,
